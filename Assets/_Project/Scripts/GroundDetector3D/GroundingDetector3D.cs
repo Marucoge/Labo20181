a1d4ba80ffae1541ec411a85a82cf5ee7674fdc8
Marucoge <airfromcountryderry@gmail.com>
2018-03-27 00:32:09 +0900

    GroundDetector3D をインポートして動作確認をした。

---
+++ Assets/_Project/Scripts/GroundDetector3D/GroundingDetector3D.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace Labo{
+    public interface IGroundingDetector3D {
+        /// <summary>
+        ///  SphereCast の結果。基本的には使わなくていいはず。
+        /// </summary>
+        RaycastHit HitInfo { get; }
+        /// <summary>
+        /// 足場となっているオブジェクト。空中にいる間は null が入る。
+        /// </summary>
+        GameObject DetectedGround { get; }
+        bool IsGrounding { get; }
+    }
+
+
+    // 未対応
+    // SphereCast 開始点のオブジェクトを検知 (開始点を手前気味に設定する or CheckSphere/OverlapSphere を使用する
+
+
+    /// <summary>
+    /// 接地判定のためのコンポーネント。
+    /// </summary>
+    public class GroundingDetector3D : MonoBehaviour, IGroundingDetector3D{
+        public RaycastHit HitInfo { get; private set; }
+        public GameObject DetectedGround { get; private set; }
+        public bool IsGrounding { get; private set; }
+        private int layermask = ~0;
+
+
+        private void Start() {
+            layermask = LayerMaskGenerator.Generate(IgnoreLayer.Specified, this.gameObject.layer);
+        }
+
+
+        private void Update() {
+            HitInfo = Cast(this.transform);
+            IsGrounding = (HitInfo.collider != null);
+            DetectedGround =
+                IsGrounding ? HitInfo.collider.gameObject : null;
+            // Debug.Log("Grounding: " + IsGrounding + " " + DetectedGround);
+        }
+
+
+        private RaycastHit Cast(Transform caster) {
+            Vector3 offsetToCastOrigin = Vector3.down * 0.2f;
+            Vector3 castOrigin = caster.transform.position + offsetToCastOrigin;
+            Vector3 castDirection = Vector3.down;
+            float castLength = 0.2f;
+            float castRadius = 0.5f;
+
+            var hitInfo = new RaycastHit();
+            Physics.SphereCast(castOrigin, castRadius, castDirection, out hitInfo, castLength, layermask, QueryTriggerInteraction.Ignore);
+
+            return hitInfo;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/GroundDetector3D/SphereCastGizmo3D.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR  // Unity Editor 以外の環境ではコンパイルしない
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System;
+
+
+namespace NewNamespace {
+    /// <summary>
+    /// デバッグ用。SphereCast の様子を OnDrawGizmos() で描画する。
+    /// 完全に独立したクラスで、SphereCaster の値を参照さえしていない(値は手動で設定する必要がある)。
+    /// </summary>
+    public class SphereCastGizmo3D : MonoBehaviour {
+        [SerializeField] private bool IsEnable = true;
+        [SerializeField] private Vector3 Offset = new Vector3(0, 0, 0);
+        [SerializeField] private float Radius = 0.5f;
+        [SerializeField] private float Length = 10.0f;
+
+
+        // ギズモが描画されている間なら実行停止していても呼ばれ続けるので注意。
+        private void OnDrawGizmos() {
+            if (IsEnable == false) { return; }
+            if (UnityEditor.EditorApplication.isPlaying == false) { return; } // エディタが実行中の場合のみ処理を行うように。
+
+            Vector3 origin = transform.position + Offset;
+            Vector3 direction = Vector3.down;
+            int layermask = ~0;
+            var hitInfo = new RaycastHit();
+
+            bool isHit =
+                Physics.SphereCast(origin, Radius, direction, out hitInfo, Length, layermask, QueryTriggerInteraction.Ignore);
+
+            // 何にもヒットしなかった場合、レイキャストの最大距離まで伸び切った様子を描画する。
+            if (isHit == false) {
+                Gizmos.color = new UnityEngine.Color32(255, 0, 0, 255);      // Red
+                Gizmos.DrawRay(origin, Vector3.down * Length);
+                Gizmos.DrawWireSphere(origin + (direction * Length), Radius);
+                return;
+            }
+
+            // ヒットしたら、その Sphere をその場所に描画。
+            Gizmos.color = new UnityEngine.Color32(0, 255, 255, 255);      // Cyan
+            Gizmos.DrawRay(origin, Vector3.down * hitInfo.distance);
+            Gizmos.DrawWireSphere(origin + (direction * hitInfo.distance), Radius);
+        }
+    }
+}
+
+#endif  // Unity Editor 以外の環境ではコンパイルしない
