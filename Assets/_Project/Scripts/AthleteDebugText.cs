40214e52de2854f34897ed03b1a2b93d2fc8e003
Marucoge <airfromcountryderry@gmail.com>
2018-04-01 13:59:41 +0900

    AthleteDebugText を作った。接地判定の状態などを可視化する。メインの処理に全く影響を与えたくなかったのでコード自体はあまり綺麗じゃない。

---
+++ Assets/_Project/Scripts/AthleteDebugText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+//using UnityEditor;
+using UnityEngine;
+using System;
+using System.Text;
+
+
+namespace Labo{
+    public class AthleteDebugText : MonoBehaviour{
+        [SerializeField] private Text DebugText;
+        [SerializeField] private GroundingDetector3D detector;
+        private StringBuilder builder;
+
+
+        private void Start() {
+            builder = new StringBuilder();
+            builder.Capacity = 512;
+        }
+
+
+        private void Update() {
+            if(DebugText == null) { return; }
+            if (detector == null) { return; }
+            builder.Length = 0;     // 文字列のリセット。
+
+            InformAboutGrounding();
+
+            DebugText.text = builder.ToString();
+        }
+
+
+        private void InformAboutGrounding() {
+            builder.Append("Grounding: ");
+            builder.Append(detector.IsGrounding);
+
+            if (detector.DetectedGround == null) {
+                builder.Append(" on null");
+                AppendNewLine();
+                return;
+            }
+
+            // 接地している場合。
+            // GameObjectをそのまま渡すと引数として bool が渡されたことになってしまう。対処方法がわからない。
+            builder.Append(" on ");
+            builder.Append(detector.DetectedGround.ToString());
+            AppendNewLine();
+        }
+
+
+        private void AppendNewLine() {
+            builder.Append(Environment.NewLine);
+        }
+    }
+}
