94b4148710d3b05091b10df1f942dd1ea4f51e62
Marucoge <airfromcountryderry@gmail.com>
2018-04-08 16:55:27 +0900

    AutomaticRotatorスクリプトと、Blenderで作った坂オブジェクトをインポートした。

---
+++ Assets/KinematicLabo/AutomaticRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace Labo{
+    [RequireComponent(typeof(Rigidbody))]   // 注:物理挙動を使わない場合でも必要。Kinematicとして使う。
+    public class AutomaticRotator : MonoBehaviour{
+        [SerializeField] private Vector3 RotationPerSecond = new Vector3(0, 360, 0);
+        
+        private void Update() {
+            Vector3 rotationPerFrame = RotationPerSecond * Time.deltaTime;
+            this.transform.Rotate(rotationPerFrame);
+        }
+
+    }
+}
